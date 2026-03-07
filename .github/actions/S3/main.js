5c04bb6b9ee20149bf467a88266514ff75c75d0b
raviteja <achyutha.teja@gmail.com>
2026-03-07 21:57:28 +0530

    added JS custom action for S3

---
+++ .github/actions/S3/main.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec')
+
+function run(){
+    // Get Input Values from workflow
+    const bucketName = core.getInput('bucket-name', {required: true})
+    const bucketRegion = core.getInput('bucket-region', {required: true})
+    const distFolder = core.getInput('dist-folder', {required: true})
+
+    // Upload Files
+    const S3uri = `s3://${bucketName}`
+    exec.exec(`aws s3 sync ${distFolder} ${S3uri} --region ${bucketRegion}`)
+
+}
+
+run();
